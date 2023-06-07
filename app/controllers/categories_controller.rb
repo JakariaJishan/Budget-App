0eca2cfc320e777c0f1ff1202aebad36f083b6ff
JakariaJishan <jakariajishan987@gmail.com>
2023-06-08 04:48:52 +0600

    Style categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,10 @@
 class CategoriesController < ApplicationController
     load_and_authorize_resource
     before_action :authenticate_user!
+
     def index
         @categories = Category.where(user_id:current_user.id)
+        @total_amount = Expense.where(user_id:current_user.id).sum(:amount)
     end
 
     def show
