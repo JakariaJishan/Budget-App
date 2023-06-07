2b892f1c67c9a359cd5125ec9e354abbbd9c0846
JakariaJishan <jakariajishan987@gmail.com>
2023-06-07 23:02:12 +0600

    Add new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,28 @@
 class CategoriesController < ApplicationController
+    before_action :authenticate_user!
     def index
-        @categories = Category.all
+        @categories = Category.where(user_id:current_user.id)
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        @category.user_id = current_user.id
+
+        if @category.save
+            redirect_to categories_path, notice:"Category added successfully"
+        else
+            redirect_to new_category_path, notice:"Failed to add category"
+        end
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name, :icon)
     end
 
 end
